a5183dba9bf2c6b984b545d4e30b01588242525a
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-09-05 09:34:08 +0900

    rustbox hello

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
-extern crate ncurses;
+extern crate rustbox;
 
-use ncurses::*;
+use std::default::Default;
+
+use rustbox::{Color, RustBox};
+use rustbox::Key;
 
 fn main() {
-    // init screen and print Hello World at top-left, wait-key, exit
-    initscr();
-    printw("Hello World");
-    refresh();
-    getch();
-    endwin();
+    let rustbox = match RustBox::init(Default::default()) {
+        Result::Ok(v) => v,
+        Result::Err(e) => panic!("{}", e),
+    };
+
+    rustbox.print(0, 0, rustbox::RB_NORMAL, Color::White, Color::Default, "Hello, world!");
+    rustbox.print(0, 1, rustbox::RB_NORMAL, Color::White, Color::Default, "press q to quit");
+
+    loop {
+        rustbox.present();
+        match rustbox.poll_event(false) {
+            Ok(rustbox::Event::KeyEvent(key)) => {
+                match key {
+                    Key::Char('q') => { break; }
+                    _ => {}
+                }
+            },
+            Err(e) => panic!("{}", e),
+            _ => {}
+        }
+    }
 }
